0dc1cc629c4d2425f37f89e2aa3dcc4297bb8d4e
Rahul Chavan <105979644+rahulcdev13@users.noreply.github.com>
2023-10-19 23:55:56 +0530

    Setup done for redux

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SidebarDrawer from "./SidebarDrawer";
+
+const Home = () => {
+  return (
+    <>
+    <SidebarDrawer />
+    </>
+  );
+};
+
+export default Home;

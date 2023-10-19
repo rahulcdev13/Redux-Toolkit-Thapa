effa23cb92c4fdc1f3881cfa45b019f7f3bf7d11
Rahul Chavan <105979644+rahulcdev13@users.noreply.github.com>
2023-10-20 01:02:19 +0530

    Samat

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 function App() {
   return (
       <div className="App">
-    <h1>Heloo rahul chavan</h1>
+    <h1>Heloo rahul chavan Samat</h1>
       <Home />
     </div>
   );
